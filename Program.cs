ad1e8d450e79a15c8f1da68c6909977e33de3e92
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-26 22:47:40 +0530

    [Naveen] Added a Regex code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration user = new UserRegistration();
+            user.Validation();
+            Console.ReadLine();
+        }
+    }
+}
